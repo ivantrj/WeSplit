661f6b145509a2bd6e9f9076d2b59c83eafea9dc
Ivan Trj <ivantrajanovski@gmail.com>
2023-05-28 08:23:59 +0200

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Ivan Trajanovski on 28.05.23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
